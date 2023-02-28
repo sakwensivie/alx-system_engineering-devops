c0f315330795dbf065bd6f9717c29214c8e6515a
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-28 16:33:31 +0000

    adding some comments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,15 @@ import sys
 
 def number_of_subscribers(subreddit):
     """ Queries to Reddit API """
+    # User-Agent is a required header field that identifies the user agent
     u_agent = 'Mozilla/5.0'
 
+    # headers is a dictionary of HTTP Headers to send with the Request
     headers = {
         'User-Agent': u_agent
     }
 
+    # url is the url to send the request to
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
